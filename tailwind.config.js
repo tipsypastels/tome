35e0c636ca64b9f684001a29f61a4eb5b17ace6b
tipsypastels <dakotasankey@gmail.com>
2024-10-10 09:08:01 -0700

    prose and forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
