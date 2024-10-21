1b1ea08b4298090edd5ec59c567fc01eba9b45e5
tipsypastels <dakotasankey@gmail.com>
2024-10-20 19:01:28 -0700

    base layout imports tw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import { Head, Container } from "$lib/components/layout";
   import type { ChildrenProps } from "$lib/props";
+  import "../app.css";
 
   let { children }: ChildrenProps = $props();
 </script>
 
-<Head />
-
-<Container>
-  {@render children()}
-</Container>
+{@render children()}
